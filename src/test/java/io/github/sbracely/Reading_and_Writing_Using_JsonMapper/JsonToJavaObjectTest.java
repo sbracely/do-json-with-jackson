764b2b6f0427611660b56fe085ed2e7851f7f8cc
sbracely <sbrace0822@gmail.com>
2026-09-06 18:09:08 +0800

    update creatingAJavaListFromAJsonArrayString

--- src/test/java/io/github/sbracely/Reading_and_Writing_Using_JsonMapper/JsonToJavaObjectTest.java
+++ src/test/java/io/github/sbracely/Reading_and_Writing_Using_JsonMapper/JsonToJavaObjectTest.java
@@ -1,11 +1,15 @@
 package io.github.sbracely.Reading_and_Writing_Using_JsonMapper;
 
 import org.junit.jupiter.api.Test;
+import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.json.JsonMapper;
+import tools.jackson.databind.type.CollectionType;
 
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,5 +23,49 @@ public class JsonToJavaObjectTest {
         Car car = jsonMapper.readValue(path, Car.class);
         assertThat(car.getColor()).isEqualTo("red");
         assertThat(car.getType()).isEqualTo("sedan");
+    }
+
+    @Test
+    void creatingAJavaListFromAJsonArrayString() {
+        String jsonCarArray = """
+                [
+                  {
+                    "color": "Black",
+                    "type": "BMW"
+                  },
+                  {
+                    "color": "Red",
+                    "type": "FIAT"
+                  }
+                ]
+                """;
+        JsonMapper jsonMapper = JsonMapper.builder().build();
+
+        // 1: Using readerForListOf
+        List<Car> carList1 = jsonMapper.readerForListOf(Car.class).readValue(jsonCarArray);
+
+        // 2: Using TypeReference
+        TypeReference<List<Car>> typeReference = new TypeReference<>() {
+        };
+        List<Car> carList2 = jsonMapper.readValue(jsonCarArray, typeReference);
+
+        // 3: Using CollectionType
+        CollectionType collectionType = jsonMapper.getTypeFactory().constructCollectionType(List.class, Car.class);
+        List<Car> carList3 = jsonMapper.readValue(jsonCarArray, collectionType);
+
+        // 4: Using an array
+        Car[] car = jsonMapper.readValue(jsonCarArray, Car[].class);
+        List<Car> carList4 = Arrays.stream(car).toList();
+
+        assertThat(carList1).hasSize(2);
+
+        assertThat(carList1.get(0).getColor()).isEqualTo("Black");
+        assertThat(carList1.get(0).getType()).isEqualTo("BMW");
+        assertThat(carList1.get(1).getColor()).isEqualTo("Red");
+        assertThat(carList1.get(1).getType()).isEqualTo("FIAT");
+
+        assertThat(carList2).usingRecursiveComparison().isEqualTo(carList1);
+        assertThat(carList3).usingRecursiveComparison().isEqualTo(carList1);
+        assertThat(carList4).usingRecursiveComparison().isEqualTo(carList1);
     }
 }
