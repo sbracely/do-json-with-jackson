5de2d5b328ddcb490225f5eed1c220ed92fb1f71
sbracely <sbrace0822@gmail.com>
2026-09-06 17:46:29 +0800

    add JsonToJacksonJsonNode

---
+++ src/test/java/io/github/sbracely/Reading_and_Writing_Using_JsonMapper/JsonToJacksonJsonNode.java
@@ -0,0 +1,24 @@
+package io.github.sbracely.Reading_and_Writing_Using_JsonMapper;
+
+import org.junit.jupiter.api.Test;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.json.JsonMapper;
+
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JsonToJacksonJsonNode {
+    @Test
+    void readsJsonNodeFromJsonFile() throws URISyntaxException {
+        Path path = Paths.get(Objects.requireNonNull(getClass().getClassLoader().getResource("car.json")).toURI());
+        JsonMapper jsonMapper = JsonMapper.builder().build();
+        JsonNode jsonNode = jsonMapper.readTree(path);
+        IO.println(jsonNode.toPrettyString());
+        assertThat(jsonNode.get("color").asString()).isEqualTo("red");
+        assertThat(jsonNode.get("type").asString()).isEqualTo("sedan");
+    }
+}
