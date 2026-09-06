b997b21efc79904282f05199ad45e95da22434b4
sbracely <sbrace0822@gmail.com>
2026-09-06 18:22:18 +0800

    add creatingJavaMapFromJsonString

--- src/test/java/io/github/sbracely/Reading_and_Writing_Using_JsonMapper/JsonToJavaObjectTest.java
+++ src/test/java/io/github/sbracely/Reading_and_Writing_Using_JsonMapper/JsonToJavaObjectTest.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.Test;
 import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.json.JsonMapper;
 import tools.jackson.databind.type.CollectionType;
+import tools.jackson.databind.type.MapType;
 
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -67,5 +69,39 @@ public class JsonToJavaObjectTest {
         assertThat(carList2).usingRecursiveComparison().isEqualTo(carList1);
         assertThat(carList3).usingRecursiveComparison().isEqualTo(carList1);
         assertThat(carList4).usingRecursiveComparison().isEqualTo(carList1);
+    }
+
+    @Test
+    void creatingJavaMapFromJsonString() {
+        String jsonCarMap = """
+                {
+                  "car1": {
+                    "color": "Black",
+                    "type": "BMW"
+                  },
+                  "car2": {
+                    "color": "Red",
+                    "type": "FIAT"
+                  }
+                }
+                """;
+        JsonMapper jsonMapper = JsonMapper.builder().build();
+
+        // 1: Using  TypeReference
+        TypeReference<Map<String, Car>> typeReference = new TypeReference<>() {
+        };
+        Map<String, Car> carMap1 = jsonMapper.readValue(jsonCarMap, typeReference);
+
+        // 2: Using MapType
+        MapType mapType = jsonMapper.getTypeFactory().constructMapType(Map.class, String.class, Car.class);
+        Map<String, Car> carMap2 = jsonMapper.readValue(jsonCarMap, mapType);
+
+        assertThat(carMap1).hasSize(2);
+        assertThat(carMap1.get("car1").getColor()).isEqualTo("Black");
+        assertThat(carMap1.get("car1").getType()).isEqualTo("BMW");
+        assertThat(carMap1.get("car2").getColor()).isEqualTo("Red");
+        assertThat(carMap1.get("car2").getType()).isEqualTo("FIAT");
+
+        assertThat(carMap2).usingRecursiveComparison().isEqualTo(carMap1);
     }
 }
